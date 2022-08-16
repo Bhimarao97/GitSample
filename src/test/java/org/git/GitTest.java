b99e3afbd648aacea75a75df9803a35112fdad78
Bhimarao97 <beemaanderson776@gmail.com>
2022-08-16 21:13:37 +0530

    Added Class1

---
+++ src/test/java/org/git/GitTest.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class GitTest {
+	
+	private void m1() {
+System.out.println("1");
+	}
+	private void m2() {
+		System.out.println("2");
+
+	}
+	private void m3() {
+		System.out.println("3");
+
+	}
+
+}
